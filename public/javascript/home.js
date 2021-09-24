11d378b1736671c0d736715a3d51fb652d5e42f3
Travis Russell <mtrussell@gmail.com>
2021-09-23 21:39:13 -0600

    fixed text field validation and added comments for clarity

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -2,13 +2,16 @@ async function newComment(e) {
     e.preventDefault();
     // target will be undefined if the button clicked was not the 'Comment' button, otherwise it will be the article ID
     const target = parseInt(e.target.id.split(':')[1]);
+    // retrieve the text from the comment field
     const commentBody = $('#commentText' + target).val();
-    if (target != undefined) {
-        if (commentBody === '') {
-            alert('Please enter a comment before submitting.');
-            return;
-        }
-    } else {
+    // evaluates to true if the button clicked was the 'Comment' button but there was no text in the entry field
+    if (target != undefined && commentBody === '') {
+        alert('Please enter a comment before submitting.');
+        return;
+    } 
+    // if the 'Comment' button is clicked and there is text in the entry field, POST the comment and
+    // reload the dashboard
+    else if (target != undefined && commentBody != '') {
         const response = await fetch('/api/comment/', {
             method: 'post',
             body: JSON.stringify({
@@ -20,5 +23,5 @@ async function newComment(e) {
         window.location.replace('/home');
     }
 }
-
+// button click handler
 $('button').on('click', newComment);
